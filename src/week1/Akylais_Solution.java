7faa8a0dfbd0d0ce6fc0bdb3a9b97e884531d8d9
Akylaisyi <122125748+Akylaisyi@users.noreply.github.com>
2023-05-29 14:52:49 -0500

    Update Akylais_Solution.java
    first two Java tasks

--- src/week1/Akylais_Solution.java
+++ src/week1/Akylais_Solution.java
@@ -1,4 +1,31 @@
 package week1;
 
 public class Akylais_Solution {
+  public static String OddEven(int num){
+        String result="";
+       if(num % 2 == 0){
+           result="even";
+       }else if(num % 2 == 1){
+           result="odd";
+       }
+       return result;
+    }
+    public static int divide(int dividend, int divisor){
+        int q=0;
+        boolean isDivPos=(dividend>=0)? true:false;
+        boolean isDivisor=(divisor>=0)?true:false;
+        if(divisor==0)
+            try {
+                throw new Exception("Zero as divisor!");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        dividend=Math.abs(dividend);
+        divisor=Math.abs(divisor);
+        while (divisor<=dividend){
+            dividend-=divisor;
+            q++;
+        }
+        return (isDivPos^isDivisor)?-q:q;
+    }
 }
