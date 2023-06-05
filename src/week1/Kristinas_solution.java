c4733ea34ca4141298dc19c9aa7b5f3d3a53b870
Krislev94 <Kristinalevinskaia@gmail.com>
2023-06-04 23:57:52 -0400

    added division method

--- src/week1/Kristinas_solution.java
+++ src/week1/Kristinas_solution.java
@@ -9,8 +9,14 @@ public class Kristinas_solution {
         // identify(0);
 
         finra();
+
+        System.out.println();
+
+        System.out.println("divide(30,10) = " + divide(30, 10));
+        System.out.println("divide(50,5) = " + divide(50, 5));
     }
 
+    // Identify odd or even number
         public static void identify(int number){
 
             if(number <= 0) {
@@ -24,7 +30,33 @@ public class Kristinas_solution {
             }
         }
 
+        // Divide numbers without / operator
+        // Write a method that can divide two numbers without using division operator
 
+    public static int divide(int num1, int num2){
+
+        if (num2 == 0){ // we can't divide on zero
+            throw new ArithmeticException();
+        } else if (num1 == 0) { // if we divide zero on any number we will get 0
+            return 0;
+        }
+
+        int result = 0;
+        int count = 0;
+
+        while (num1 >= num2){
+            num1 -= num2;
+            count++;
+        }
+
+        return count;
+
+    }
+
+
+        // Print FINRA if the number is divisible by 3 and 5
+        // Print FIN if the number is divisible by 3
+        // // Print RA if the number is divisible by 5
     public static void finra(){
 
         for( int i =1; i <= 30; i++){
