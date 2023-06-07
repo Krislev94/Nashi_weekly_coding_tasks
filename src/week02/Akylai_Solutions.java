f74a7cdc689ad161e466b357a65bb2fcb8d94c77
akyla <akylaisyi@yahoo.com>
2023-06-07 10:31:32 -0500

    Added my solution

--- src/week02/Akylai_Solutions.java
+++ src/week02/Akylai_Solutions.java
@@ -1,40 +1,73 @@
 package week02;
 
+import java.util.ArrayList;
+
 public class Akylai_Solutions {
     public static void main(String[] args) {
-consecutiveNum(17);
-swapNum(10,3);
+
+        swapNum(10, 3);
+        divisibleNy3_5_15();
+        consecutiveNum(17);
 
 
     }
+    //TASK#1
+    public static void swapNum(int a, int b) {
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a=" + a + " b=" + b);
+    }
+    //TASK#2
+    public static void divisibleNy3_5_15() {
 
+        System.out.print("Divisible by 3");
+        for (int i = 1; i <= 100; i++) {
 
+            if (i % 3 == 0) {
 
-    public static void consecutiveNum(int n){
+                System.out.print(i + " ");
+            }
+        }
 
-        for (int i = 1; i <= n ; i++) {
+        System.out.println();
+        System.out.print("Divisible by 5");
+        for (int i = 1; i <= 100; i++) {
 
-            if(i%2==0){
+            if (i % 5 == 0) {
+
+                System.out.print(i + " ");
+            }
+        }
+
+        System.out.println();
+        System.out.print("Divisible By 15");
+        for (int i = 1; i <= 100; i++) {
+
+            if (i % 15 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+    //TASK3
+    public static void consecutiveNum(int n) {
+
+        for (int i = 1; i <= n; i++) {
+
+            if (i % 2 == 0) {
                 System.out.print("Codility");
             }
-            if(i%3==0){
+            if (i % 3 == 0) {
                 System.out.print("Test");
             }
-            if(i%5==0){
+            if (i % 5 == 0) {
                 System.out.print("Coders");
             }
 
-            if(!(i%2==0) && !(i%3==0) && !(i%5==0)){
+            if (!(i % 2 == 0) && !(i % 3 == 0) && !(i % 5 == 0)) {
                 System.out.print(i);
             }
             System.out.println();
         }
-    }
-
-    public static void swapNum(int a, int b){
-        a=a+b;
-        b=a-b;
-        a=a-b;
-        System.out.println("a="+a+" b="+b);
     }
 }
