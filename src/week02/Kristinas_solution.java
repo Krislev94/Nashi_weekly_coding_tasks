6f30ad6831703ddb3304ad610b88e2b3250f964c
Krislev94 <Kristinalevinskaia@gmail.com>
2023-06-12 23:53:08 -0400

    divisible_15_5_3 method created

--- src/week02/Kristinas_solution.java
+++ src/week02/Kristinas_solution.java
@@ -1,9 +1,14 @@
 package week02;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Kristinas_solution {
     public static void main(String[] args) {
         swapVariables(45,10);
         swapVariables(23,54);
+
+        divisibleBy_15_5_3();
     }
 
     public static void swapVariables(int x, int y){
@@ -16,5 +21,38 @@ public class Kristinas_solution {
         System.out.println("x = " + x);
         System.out.println("y = " + y);
 
+    }
+
+    public static void divisibleBy_15_5_3(){
+
+        List<Integer> divisible15 = new ArrayList<>();
+        List<Integer> divisible5 = new ArrayList<>();
+        List<Integer> divisible3 = new ArrayList<>();
+
+        for (int i = 1; i < 101 ; i++) {
+            if (i % 15 == 0){
+               divisible15.add(i);
+            } else if (i % 5 == 0) {
+                divisible5.add(i);
+            }else if (i % 3 == 0){
+               divisible3.add(i);
+            }
+        }
+
+        String num15 = divisible15.toString();
+        num15 = num15.substring(1,num15.length()-1);
+
+        String num5 = divisible5.toString();
+        num5 = num5.substring(1,num5.length()-1);
+
+        String num3 =divisible3.toString();
+        num3 = num3.substring(1, num3.length()-1);
+
+
+
+
+        System.out.println("Divisible by 15 : " + num15);
+        System.out.println("Divisible by 5 : " + num5);
+        System.out.println("Divisible by 3 : " + num3);
     }
 }
