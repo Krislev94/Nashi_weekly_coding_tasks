ae05b7001261b8a10ebd52832c3740d8de1d8ad2
Krislev94 <Kristinalevinskaia@gmail.com>
2023-06-16 18:07:43 -0400

    Prime method comments

--- src/week03/Kristinas_Solution.java
+++ src/week03/Kristinas_Solution.java
@@ -12,15 +12,20 @@ public class Kristinas_Solution {
 
     public static boolean isPrimeNumber(int number){
 
+        // NEGATIVE NUMBERS CANNOT BE PRIME
         if (number < 0){
             throw new ArithmeticException("Negative numbers cannot be prime");
         }
 
+        // ZERO CANNOT BE PRIME
         if (number == 0){
             throw new ArithmeticException("Zero is not a prime number");
         }
 
+        // DIVIDING THE GIVEN NUMBER BY EACH NUMBER STARTING FROM TO UNTIL THE GIVEN NUMBER
         for (int i = 2; i < number ; i++) {
+
+            // If it's divisible by any number then it's not a prime
             if (number % i == 0){
                 return false;
             }
