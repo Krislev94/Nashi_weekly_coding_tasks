1a7e1e37b1a431799aced5b0182e9577bbc492c5
Krislev94 <Kristinalevinskaia@gmail.com>
2023-06-16 19:18:46 -0400

    remove Dupliates method created

--- src/week04/Kristinas_solution.java
+++ src/week04/Kristinas_solution.java
@@ -1,4 +1,28 @@
 package week04;
 
 public class Kristinas_solution {
+
+    public static void main(String[] args) {
+        removeDuplicates("AAABBBTTAAAMMBKJTFN");
+    }
+
+    public static void removeDuplicates(String string){
+
+
+        String result = "";
+
+        for (int i = 0; i < string.length(); i++) {
+
+            for (int j = string.length()-1; j >= 0 ; j--) {
+
+                if (!result.contains(""+string.charAt(i))){
+                    result += string.charAt(i);
+                }
+            }
+
+        }
+        System.out.println(result);
+
+
+    }
 }
