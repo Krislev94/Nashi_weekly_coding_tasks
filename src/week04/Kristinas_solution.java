d2617aaa816940b52d5c8041b57e015bfff4ea77
Krislev94 <Kristinalevinskaia@gmail.com>
2023-06-17 00:29:43 -0400

    frequency is done

--- src/week04/Kristinas_solution.java
+++ src/week04/Kristinas_solution.java
@@ -1,9 +1,47 @@
 package week04;
 
+
 public class Kristinas_solution {
 
     public static void main(String[] args) {
         removeDuplicates("AAABBBTTAAAMMBKJTFN");
+
+        frequencyOfCharacters("aaakkffrv");
+    }
+
+
+    public static void frequencyOfCharacters(String str){
+
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            int count = 0;
+
+            for (int j = 0; j < str.length(); j++) {
+
+                if (str.charAt(j) == str.charAt(i)){
+                    count++;
+                }
+            }
+
+            if (result.contains(""+str.charAt(i))){
+                continue;
+            }
+
+            result+= ""+str.charAt(i);
+            result += count;
+
+        }
+
+        System.out.println(result);
+
+    }
+
+    public static boolean sameLetters(String string1, String string2){
+
+        return false;
+
+
     }
 
     public static void removeDuplicates(String string){
